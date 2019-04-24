e6d0889818304cc1aced7f6bcf768b1ac6d4086f
Big P <dolphindevlopers@gmail.com>
2019-04-25 01:19:51 +0530

    user-fix-xs-hide

--- app/components/dashboard/user.js
+++ app/components/dashboard/user.js
@@ -1,5 +1,5 @@
 import React from 'react';
 
 export default () => (
-  <div><img src="https://api.adorable.io/avatars/50/abott@adorable.io.png" className="rounded-circle cp" width="50px" height="50"/></div>
+  <div className="d-none d-lg-block"><img src="https://api.adorable.io/avatars/50/abott@adorable.io.png" className="rounded-circle cp" width="50px" height="50"/></div>
 );
