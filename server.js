4c3d6602d9547d02003f06b390c77a02d4d6895c
Big P <dolphindevlopers@gmail.com>
2019-05-10 16:35:27 +0530

    landing page

--- server.js
+++ server.js
@@ -7,10 +7,17 @@ const app = express();
 
 // serve static assets normally
 app.use(express.static(__dirname));
+app.use(express.static(path.resolve(__dirname, 'landing-page/build')));
 
-app.get('*', function (request, response){
+app.get('/', (request, response) => {
+  response.sendFile(path.resolve(__dirname, 'landing-page/build/index.html'));
+});
+
+
+
+app.get('*', (request, response) => {
   response.sendFile(path.resolve(__dirname, 'dist/index.html'))
-})
+});
 
 app.listen(port)
 console.log(`server started on port ${port}`)
